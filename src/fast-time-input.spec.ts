85f93dc86de771bde8218f9de5e20c59dd0f653a
harry-sm <harrysmith1991@gmail.com>
2019-04-30 17:35:20 -0400

    (style): apply prettier formating

--- src/fast-time-input.spec.ts
+++ src/fast-time-input.spec.ts
@@ -1,54 +1,54 @@
 import * as fti from "./fast-time-input";
 
 test("basic", () => {
-  expect(fti.parse("130p")).toBe("01:30 PM");
+    expect(fti.parse("130p")).toBe("01:30 PM");
 });
 
 test("single digit", () => {
-  expect(fti.parse("7")).toBe("07:00 AM");
+    expect(fti.parse("7")).toBe("07:00 AM");
 });
 
 test("double digit", () => {
-  expect(fti.parse("11")).toBe("11:00 AM");
+    expect(fti.parse("11")).toBe("11:00 AM");
 });
 
 test("double digit past 12 should be pm", () => {
-  expect(fti.parse("13")).toBe("01:00 PM");
+    expect(fti.parse("13")).toBe("01:00 PM");
 });
 
 test("12 by default should be 12::00 PM", () => {
-  expect(fti.parse("12")).toBe("12:00 PM");
+    expect(fti.parse("12")).toBe("12:00 PM");
 });
 
 test("12 am should be 00:00 am", () => {
-  expect(fti.parse("12a")).toBe("00:00 AM");
+    expect(fti.parse("12a")).toBe("00:00 AM");
 });
 
 test("545p should be 05:45 PM", () => {
-  expect(fti.parse("545p")).toBe("05:45 PM");
+    expect(fti.parse("545p")).toBe("05:45 PM");
 });
 
 test("cap minutes to 59", () => {
-  expect(fti.parse("290")).toBe("02:59 AM");
+    expect(fti.parse("290")).toBe("02:59 AM");
 });
 
 test("invalid characters", () => {
-  expect(fti.parse("*^#H87h(@H9qp/i|w")).toBe("08:59 PM");
+    expect(fti.parse("*^#H87h(@H9qp/i|w")).toBe("08:59 PM");
 });
 
 test("24 hour time parse to 12 hour format", () => {
-  expect(fti.parse("17:00")).toBe("05:00 PM");
+    expect(fti.parse("17:00")).toBe("05:00 PM");
 });
 
 describe("Parse with format param 24hr", () => {
-  test("17p should parse to 17:00", () => {
-    expect(fti.parse("17p", "24hr")).toBe("17:00");
-  });
-  test("5p should parse to 17:00", () => {
-    expect(fti.parse("5p", "24hr")).toBe("17:00");
-  });
+    test("17p should parse to 17:00", () => {
+        expect(fti.parse("17p", "24hr")).toBe("17:00");
+    });
+    test("5p should parse to 17:00", () => {
+        expect(fti.parse("5p", "24hr")).toBe("17:00");
+    });
 
-  test("530p should parse to 17:30", () => {
-    expect(fti.parse("530p", "24hr")).toBe("17:30");
-  });
+    test("530p should parse to 17:30", () => {
+        expect(fti.parse("530p", "24hr")).toBe("17:30");
+    });
 });
--- src/fast-time-input.ts
+++ src/fast-time-input.ts
@@ -2,58 +2,58 @@
 
 // https://stackoverflow.com/questions/2998784/how-to-output-integers-with-leading-zeros-in-javascript
 function pad(num: number, size: number) {
-  var s = num + "";
-  while (s.length < size) s = "0" + s;
-  return s;
+    var s = num + "";
+    while (s.length < size) s = "0" + s;
+    return s;
 }
 
 // https://stackoverflow.com/questions/11409895/whats-the-most-elegant-way-to-cap-a-number-to-a-segment
 function clamp(val: number, min: number, max: number) {
-  return Math.min(Math.max(val, min), max);
+    return Math.min(Math.max(val, min), max);
 }
 
 // time data in 24 hours
 class Time {
-  hours = 0;
-  mins = 0;
+    hours = 0;
+    mins = 0;
 
-  constructor(hr: number, mn: number) {
-    this.hours = clamp(hr, 0, 23);
-    this.mins = clamp(mn, 0, 59);
-  }
-
-  to12Hour() {
-    let hours = this.hours;
-    let mer = "AM";
-    if (hours > 12) {
-      hours -= 12;
-      mer = "PM";
+    constructor(hr: number, mn: number) {
+        this.hours = clamp(hr, 0, 23);
+        this.mins = clamp(mn, 0, 59);
     }
-    return pad(hours, 2) + ":" + pad(this.mins, 2) + " " + mer.toUpperCase();
-  }
 
-  to24Hour() {
-    return pad(this.hours, 2) + ":" + pad(this.mins, 2);
-  }
+    to12Hour() {
+        let hours = this.hours;
+        let mer = "AM";
+        if (hours > 12) {
+            hours -= 12;
+            mer = "PM";
+        }
+        return pad(hours, 2) + ":" + pad(this.mins, 2) + " " + mer.toUpperCase();
+    }
+
+    to24Hour() {
+        return pad(this.hours, 2) + ":" + pad(this.mins, 2);
+    }
 }
 
 export class FastTimeInput {
-  public static parse(timeString: string, format?: "24hr" | "12hr") {
-    let newTime = "";
-    // https://stackoverflow.com/questions/2031085/how-can-i-check-if-string-contains-characters-whitespace-not-just-whitespace
-    if (/\S/.test(timeString) || timeString !== "") {
-      const t = FastTimeInput.convertInput(timeString);
-      if (!format || format === "12hr") {
-        newTime = t.to12Hour();
-      } else {
-        newTime = t.to24Hour();
-      }
+    public static parse(timeString: string, format?: "24hr" | "12hr") {
+        let newTime = "";
+        // https://stackoverflow.com/questions/2031085/how-can-i-check-if-string-contains-characters-whitespace-not-just-whitespace
+        if (/\S/.test(timeString) || timeString !== "") {
+            const t = FastTimeInput.convertInput(timeString);
+            if (!format || format === "12hr") {
+                newTime = t.to12Hour();
+            } else {
+                newTime = t.to24Hour();
+            }
+        }
+
+        return newTime;
     }
 
-    return newTime;
-  }
-
-  /*
+    /*
   1p => 1:00 PM
   12p => 12:00 PM
   13p => 1:00 PM
@@ -75,102 +75,89 @@ export class FastTimeInput {
 
   - if no p is present, AM is implied
   */
-  private static convertInput(timeString: string) {
-    var meridian = this.findMeridian(timeString);
+    private static convertInput(timeString: string) {
+        var meridian = this.findMeridian(timeString);
 
-    // strip extra characters
-    timeString = timeString.replace(/\D/g, "");
+        // strip extra characters
+        timeString = timeString.replace(/\D/g, "");
 
-    if (timeString.length == 0) return new Time(0, 0);
-    if (timeString.length == 1 || timeString.length == 2)
-      return this.convertSingleAndDouble(timeString, meridian);
-    if (timeString.length == 3) return this.convertTriple(timeString, meridian);
-    if (timeString.length == 4)
-      return this.convertQuadruple(timeString, meridian);
+        if (timeString.length == 0) return new Time(0, 0);
+        if (timeString.length == 1 || timeString.length == 2) return this.convertSingleAndDouble(timeString, meridian);
+        if (timeString.length == 3) return this.convertTriple(timeString, meridian);
+        if (timeString.length == 4) return this.convertQuadruple(timeString, meridian);
 
-    return new Time(0, 0);
-  }
+        return new Time(0, 0);
+    }
 
-  // returns am, pm or an empty string if neither is found
-  private static findMeridian(timeString: string): "am" | "pm" | null {
-    if (timeString.search("pm") !== -1 || timeString.search("p") !== -1)
-      return "pm";
-    if (timeString.search("am") !== -1 || timeString.search("a") !== -1)
-      return "am";
-    return null;
-  }
+    // returns am, pm or an empty string if neither is found
+    private static findMeridian(timeString: string): "am" | "pm" | null {
+        if (timeString.search("pm") !== -1 || timeString.search("p") !== -1) return "pm";
+        if (timeString.search("am") !== -1 || timeString.search("a") !== -1) return "am";
+        return null;
+    }
 
-  private static convertSingleAndDouble(
-    timeString: string,
-    meridian: "am" | "pm" | null
-  ) {
-    var value = parseInt(timeString) % 24;
-    if (meridian === "pm" && value < 12) value += 12;
-    return new Time(value % 24, 0);
-  }
+    private static convertSingleAndDouble(timeString: string, meridian: "am" | "pm" | null) {
+        var value = parseInt(timeString) % 24;
+        if (meridian === "pm" && value < 12) value += 12;
+        return new Time(value % 24, 0);
+    }
 
-  /*
+    /*
   only handles time between 1:01 and 9:99
   */
-  private static convertTriple(
-    timeString: string,
-    meridian: "am" | "pm" | null
-  ) {
-    var hours = parseInt(timeString.substr(0, 1));
-    if (meridian === "pm" && hours < 12) hours += 12;
+    private static convertTriple(timeString: string, meridian: "am" | "pm" | null) {
+        var hours = parseInt(timeString.substr(0, 1));
+        if (meridian === "pm" && hours < 12) hours += 12;
 
-    var mins = parseInt(timeString.substr(1, 3));
+        var mins = parseInt(timeString.substr(1, 3));
 
-    return new Time(hours, mins);
-  }
+        return new Time(hours, mins);
+    }
 
-  /*
+    /*
   ignores p and a
    */
-  private static convertQuadruple(
-    timeString: string,
-    meridian: "am" | "pm" | null
-  ) {
-    var hours = parseInt(timeString.substr(0, 2));
-    if (meridian === "pm" && hours < 12) hours += 12;
-    if (meridian === "am" && hours > 12) hours -= 12;
-    var mins = parseInt(timeString.substr(2, 5));
+    private static convertQuadruple(timeString: string, meridian: "am" | "pm" | null) {
+        var hours = parseInt(timeString.substr(0, 2));
+        if (meridian === "pm" && hours < 12) hours += 12;
+        if (meridian === "am" && hours > 12) hours -= 12;
+        var mins = parseInt(timeString.substr(2, 5));
 
-    return new Time(hours, mins);
-  }
-
-  // https://stackoverflow.com/questions/8282266/how-to-prevent-invalid-characters-from-being-typed-into-input-fields
-  public static inputFilter(e: any) {
-    // 0-9
-    if (e.which > 47 && e.which < 58) {
-      return false;
+        return new Time(hours, mins);
     }
 
-    // https://stackoverflow.com/questions/2353550/how-to-capture-a-backspace-on-the-onkeydown-event/2353562
-    // backspace and delete
-    if (e.which === 8 || e.which === 46) {
-      return false;
+    // https://stackoverflow.com/questions/8282266/how-to-prevent-invalid-characters-from-being-typed-into-input-fields
+    public static inputFilter(e: any) {
+        // 0-9
+        if (e.which > 47 && e.which < 58) {
+            return false;
+        }
+
+        // https://stackoverflow.com/questions/2353550/how-to-capture-a-backspace-on-the-onkeydown-event/2353562
+        // backspace and delete
+        if (e.which === 8 || e.which === 46) {
+            return false;
+        }
+
+        let key = e.key.toUpperCase();
+        // AMP
+        if (key === "A" || key === "P" || key === "M") return false;
+
+        // colon :
+        if (e.key === ":") {
+            return false;
+        }
+
+        e.preventDefault();
     }
-
-    let key = e.key.toUpperCase();
-    // AMP
-    if (key === "A" || key === "P" || key === "M") return false;
-
-    // colon :
-    if (e.key === ":") {
-      return false;
-    }
-
-    e.preventDefault();
-  }
 }
 
 export function parse(timeString: string, format?: "24hr" | "12hr"): string {
-  return FastTimeInput.parse(timeString, format);
+    return FastTimeInput.parse(timeString, format);
 }
 
 export function filterInput(e: any) {
-  return FastTimeInput.inputFilter(e);
+    return FastTimeInput.inputFilter(e);
 }
 
 //export default FastTimeInput;
